1628bfb6f22cb7bd7f7542190152f2c39b3a17bd
Johnsen92 <Johnsen92@bitbucket.org>
2019-02-26 18:28:13 +0100

    working user grid

--- code/vue/src/App.vue
+++ code/vue/src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
-    <section class="ftco-counter ftco-bg-dark img" id="section-counter" data-stellar-background-ratio="0.5">
+    <section class="ftco-counter ftco-bg-dark img" v-bind:style="{ height: appHeight + 'px' }" id="section-counter" data-stellar-background-ratio="0.5">
       <h1>Users</h1>
-      <div class="list-item-wrapper" v-on:click="selectUser(user.UserID)" v-bind:key="user.UserID" v-for="user in users">
+      <div class="list-item-wrapper" v-on:click="selectUser(user)" v-bind:key="user.UserID" v-for="user in users">
         <User v-bind:user="user"/>
+      </div>
+      <div class="list-item-wrapper">
+        <button v-on:click="postUser" style="margin-top: 3px; float:left">Add User</button>
       </div>
     </section>
   </div>
@@ -11,7 +14,17 @@
 
 <script>
 import User from './components/User.vue'
+import { GridPlugin } from '@syncfusion/ej2-vue-grids';
 
+Vue.use(GridPlugin);
+
+function indexById(arr, id, key){
+  var i;
+  for(i = 0; i < arr.length; i++){
+    if(arr[i][key] == id)
+      return i;
+  }
+}
 
 export default {
   name: 'app',
@@ -19,20 +32,65 @@ export default {
     User
   },
   methods: {
-    selectUser: function(id){
-      alert(id);
+    selectUser: function(user){
+      
+    },
+    editUser: function(user){
+      fetch(this.endpoint + "/Users", { 
+          body: JSON.stringify(user),
+          method: "PUT",
+          headers: {
+            "Content-Type" : "application/json"
+          }
+        })
+        .then((res) => {
+          if(res.status == 200)
+            this.users[indexById(this.users,user.UserID,"UserID")] = user;
+          else
+            alert("Error editing user " + user.UserName);
+        });
+    },
+    deleteUser: function(user){
+      fetch(this.endpoint + "/Users/" + user.UserID, { method: "DELETE" })
+        .then((res) => {
+          if(res.status == 200)
+            this.users.splice(indexById(this.users,user.UserID,"UserID"),1);
+          else
+            alert("Error deleting user " + user.UserName);
+        });
+      
+    },
+    postUser: function(){
+      fetch(this.endpoint + "/Users", { 
+          body: JSON.stringify({
+            "UserName" : "default",
+            "UserMail" : "default",
+            "UserPassword" : "default",
+            "UserSalt" : "default"
+          }),
+          method: "POST",
+          headers: {
+            "Content-Type" : "application/json"
+          }
+        })
+        .then(res => res.json())
+        .then((user) => {
+          this.users.push(user);
+        });
     }
   },
   mounted() {
     fetch(this.endpoint + "/Users")
       .then(response => response.json())
       .then((data) => {
+        this.appHeight = data.length*30 + 310;
         this.users = data;
       })
   },
   data() {
     return {
       endpoint: "http://localhost:50000",
+      appHeight: 400,
       users: []
     }
   }
@@ -45,11 +103,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #ffffff;
+  color: lightgray;
   margin-top: 60px;
 }
 h1 {
-  color: white;
+  color: lightgray;;
 }
 .list-item-wrapper {
   width: 50%;
--- code/vue/src/components/User.vue
+++ code/vue/src/components/User.vue
@@ -1,20 +1,46 @@
 <template>
   <div>
-    <div class="grid-cell">{{user.UserName}}</div><div class="grid-cell">{{user.UserMail}}</div>
+    <input style="width: 40%" v-on:keyup.enter="submit" v-bind:disabled="!editing" class="grid-cell" v-model="user.UserName" />
+    <input style="width: 40%" v-on:keyup.enter="submit" v-bind:disabled="!editing" class="grid-cell" v-model="user.UserMail" />
+    <button v-if="editing" v-on:click="submit" style="width: 10%" class="grid-cell">Submit</button>
+    <button v-else v-on:click="edit" style="width: 10%" class="grid-cell">Edit</button>
+    <button v-on:click="del" style="width: 10%" class="grid-cell">Delete</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "User",
-  props: ["user"]
+  props: ["user"],
+  data() {
+    return {
+      editing: false
+    }
+  },
+  methods: {
+    edit: function(){
+      this.editing = true;
+    },
+    submit: function(){
+      this.$parent.editUser(this.user);
+      this.editing = false;
+    },
+    del: function(){
+      this.$parent.deleteUser(this.user);
+    }
+  }
 }
 </script>
 
 <style scoped>
 .grid-cell {
-  width: 50%;
   border: solid 1px white;
+  float: left;
+  padding: 3px;
+}
+input {
+  background: transparent;
+  color: lightgray;
   float: left;
 }
 </style>
